52d8b427bcb1f1fcc976828fba4a9915a02a3d29
KoltsovaIA <igmtur@yandex.ru>
2023-11-26 19:57:30 +0300

    main svc first push

--- utils/src/main/java/ru/practicum/validator/StartBeforeEndDateValid.java
+++ utils/src/main/java/ru/practicum/validator/StartBeforeEndDateValid.java
@@ -3,18 +3,18 @@ package ru.practicum.validator;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target(ElementType.TYPE_USE)
+@Target({METHOD, CONSTRUCTOR})
 @Retention(RUNTIME)
 @Documented
 @Constraint(validatedBy = CheckDateValidator.class)
 public @interface StartBeforeEndDateValid {
-    String message() default "Start must be before end or not null";
+    String message() default "Start must be before end or null`s";
 
     Class<?>[] groups() default {};
 
